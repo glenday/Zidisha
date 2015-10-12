77f5d8cf9a323beb8790a085aea79b77758a085d
Alex Glenday <alexglenday@gmail.com>
2015-10-11 23:20:49 -0400

    Added more documentation to the load_Zidisha file.

--- ZidishaApp/app/load_Zidisha.py
+++ ZidishaApp/app/load_Zidisha.py
@@ -3,6 +3,11 @@ __author__ = 'alexglenday'
 
 
 def max_invites(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Maximum number of invitees allowed at once.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing maximum number of invitees indexed by time_bins_center.
+    """
     # maximum number of invitees
     df_max_invite = pd.DataFrame({'max_invites': 0.0}, index=time_bins_center)
     df_max_invite[('2014-04-01' <= df_max_invite.index) & (df_max_invite.index < '2014-06-01')] = 4
@@ -19,6 +24,11 @@ def max_invites(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
 
 
 def max_new_loan_size(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Maximum new loan size in USD.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing maximum loan size in USD indexed by time_bins_center.
+    """
     # maximum new loan
     df_max_new_loan = pd.DataFrame({'max_loan': 0.0}, index=time_bins_center)
     df_max_new_loan[('2012-02-1' <= df_max_new_loan.index) & (df_max_new_loan.index < '2012-08-01')] = 1000
@@ -37,6 +47,11 @@ def max_new_loan_size(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
 
 
 def loan_loss_reserve_fee_Kenya(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Loan loss reserve fee in USD for Kenya.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing the loan loss reserve fee in USD indexed by time_bins_center.
+    """
     # fee level for Kenya
     df_fee_level = pd.DataFrame({'fee_level': 0.0}, index=time_bins_center)
     df_fee_level[('2015-02-21' <= df_fee_level.index) & (df_fee_level.index < '2015-04-22')] = 20
@@ -59,6 +74,11 @@ def loan_loss_reserve_fee_Other(time_bins_center: pd.DatetimeIndex) -> pd.DataFr
 
 
 def loan_loss_reserve_fee_indonesia(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Loan loss reserve fee (and membership fee) in USD for Indonesia.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing the loan loss reserve fee in USD indexed by time_bins_center.
+    """
     # fee level for Indonesia
     df_fee_level = pd.DataFrame({'fee_level': 0.0}, index=time_bins_center)
     df_fee_level[('2015-02-10' <= df_fee_level.index) & (df_fee_level.index < '2015-03-06')] = 18
@@ -69,6 +89,11 @@ def loan_loss_reserve_fee_indonesia(time_bins_center: pd.DatetimeIndex) -> pd.Da
 
 
 def loan_loss_reserve_fee_ghana(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Loan loss reserve fee (and membership fee) in USD for Ghana.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing the loan loss reserve fee in USD indexed by time_bins_center.
+    """
     # fee level for Ghana
     df_fee_level = pd.DataFrame({'fee_level': 0.0}, index=time_bins_center)
     df_fee_level[('2015-02-08' <= df_fee_level.index) & (df_fee_level.index < '2015-03-06')] = 27
@@ -79,6 +104,11 @@ def loan_loss_reserve_fee_ghana(time_bins_center: pd.DatetimeIndex) -> pd.DataFr
 
 
 def website_change(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Dummy variable indicating the website change.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing the dummy variable indexed by time_bins_center.
+    """
 
     df_web_change = pd.DataFrame({'web_change': 0.0}, index=time_bins_center)
     df_web_change[('2015-01-01' <= df_web_change.index) & (df_web_change.index < '2015-10-01')] = 1
@@ -87,6 +117,11 @@ def website_change(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
 
 
 def upfront_fee(time_bins_center: pd.DatetimeIndex) -> pd.DataFrame:
+    """
+    Dummy variable indicating the use of an upfront fee.
+    :param time_bins_center: Center time value for each bin.
+    :return: DataFrame containing the dummy variable indexed by time_bins_center.
+    """
 
     df_upfront_fee = pd.DataFrame({'upfront_fee': 0.0}, index=time_bins_center)
     df_upfront_fee[('2015-02-21' <= df_upfront_fee.index) & (df_upfront_fee.index < '2015-04-08')] = 1
